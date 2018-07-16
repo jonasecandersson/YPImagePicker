154ab397aeb9382df71bf1750ac4e082b1a75a01
Jonas Andersson <jonas@appfrilans.se>
2018-07-16 09:29:39 +0200

    public

--- Source/Helpers/UIScreenMocked.swift
+++ Source/Helpers/UIScreenMocked.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-class UIScreenMocked {
-    var width: CGFloat = UIScreen.main.bounds.height
-    var height: CGFloat = UIScreen.main.bounds.height
-    var scale: CGFloat = UIScreen.main.scale
-    static let shared = UIScreenMocked()
+public class UIScreenMocked {
+    public var width: CGFloat = UIScreen.main.bounds.height
+    public var height: CGFloat = UIScreen.main.bounds.height
+    public var scale: CGFloat = UIScreen.main.scale
+    public static let shared = UIScreenMocked()
 
-    init() {
+    public init() {
     }
 }
